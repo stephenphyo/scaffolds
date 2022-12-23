91f958302ebd6e2b78e514291a9d5712767e6bce
STEPHEN PHYO <stephenphyo2019.official@gmail.com>
2022-12-23 04:18:37 +0000

    v01: Initial Creation

---
+++ README.md
@@ -0,0 +1,1 @@
+<h3>React.JS Frontend Scaffold</h3>
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/* CSS Imports */
+import 'styles/App.css';
+
+function App() {
+    return (
+        <main className='app'>
+            <section className='container'>
+            </section>
+        </main>
+    );
+}
+
+export default App;
